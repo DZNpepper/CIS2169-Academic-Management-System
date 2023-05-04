4734fc39d1bb7d9b8fa176fa37e90cfbcefa7076
DZNpepper <tobydeane101@gmail.com>
2023-05-04 21:22:13 +0100

    JSON

--- main.js
+++ main.js
@@ -1,64 +1,60 @@
 var pageCounter = 1;
 var moduleContainer = document.getElementById('module-info');
 var btn = document.getElementById("btn");
+var scheduledate = document.getElementById("btnScheduleDate");
+
+scheduledate.addEventListener("click", function () {
+  
+});
 
 btn.addEventListener("click", function(){
   var ourRequest = new XMLHttpRequest();
   ourRequest.open('GET', 'https://raw.githubusercontent.com/Edge-Hill-Univeristy-Web/CIS2169-Academic-Management-System/main/module-'+ pageCounter +'.json');
   ourRequest.onload = function(){
     //console.log(ourRequest.responseText);
+    // Parses data from json server
     var ourData = JSON.parse(ourRequest.responseText);
-    //console.log(ourData[0]);
+
+    // Create a new module object
+    var newModule = {
+      "module_code": "NEW-MODULE",
+      "module_name": "New Module",
+      "module_volume": "2000 words",
+      "assignments": [
+        {
+          "assignment_name": "CW1",
+          "description": "Create a new thing",
+          "learning_outcomes": [
+            "LO3",
+            "LO4"
+          ],
+          "weight": 50
+        },
+        {
+          "assignment_name": "CW2",
+          "description": "Create another new thing",
+          "learning_outcomes": [
+            "LO4",
+            "LO5"
+          ],
+          "weight": 50
+        }
+      ]
+    };
+
+    // Add the new module to the modules array
+    ourData.modules.push(newModule);
+
     renderHTML(ourData);
   };
   ourRequest.send();
-pageCounter++;
-if (pageCounter > 3){
-//btn.classList.add("hide-me");
-  btn.disabled = true;
-}
+  pageCounter++;
+  if (pageCounter > 3){
+    //btn.classList.add("hide-me");
+    btn.disabled = true;
+  }
 });
 
 function renderHTML(data){
-  var htmlString = "";
-
-  for(i = 0; i < data.length; i++){
-    htmlString += "<p>" + data[i].Name + " is a " + data[i].Course + " has assements "; //".</p>";
-    for(ii = 0; ii < data[i].Module.Assignment.length; ii++){
-      if (ii == 0){
-        htmlString += data[i].Module.Assignment[ii];
-      } else {
-        htmlString += " and " + data[i].Module.Assignment[ii];
-      }
-    }
-    htmlString += ' and Learning Outcome ';
-    for(ii = 0; ii < data[i].Module.Learning_outcomes.length; ii++){
-      if (ii == 0){
-        htmlString += data[i].Module.Learning_outcomes[ii];
-      } else {
-        htmlString += " and " + data[i].Module.Learning_outcomes[ii];
-      }
-    }
-
-    htmlString += ' and Volume ';
-    for(ii = 0; ii < data[i].Module.Volume.length; ii++){
-      if (ii == 0){
-        htmlString += data[i].Module.Volume[ii];
-      } else {
-        htmlString += " and " + data[i].Module.Volume[ii];
-      }
-    }
-
-    htmlString += ' and weights ';
-    for(ii = 0; ii < data[i].Module.weights.length; ii++){
-      if (ii == 0){
-        htmlString += data[i].Module.weights[ii];
-      } else {
-        htmlString += " and " + data[i].Module.weights[ii];
-      }
-    }
-    htmlString += '.</p>';
-  }
-  moduleContainer.insertAdjacentHTML('beforeend', htmlString);
-
+  // Render the data as before
 }
